1f67f802573681e415f14ace6946f144ed505ef3
Jordao Serafim <jordaoesa@gmail.com>
2024-08-22 23:40:00 -0400

    tweaks to service workers

--- src/index.js
+++ src/index.js
@@ -1,73 +1,13 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-//
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-//
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-
-// index.js
-//
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-//
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <App />
-//     </React.StrictMode>,
-//     document.getElementById('root')
-// );
-
-// Ativar o service worker para suporte offline
-// serviceWorkerRegistration.register();
-//
-//
-// // import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-//
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//     <React.StrictMode>
-//         <App />
-//     </React.StrictMode>
-// );
-//
-// reportWebVitals();
-// // Ativar o service worker para suporte offline
-// serviceWorkerRegistration.register();
-
-
-
-
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
-// Ativar o service worker
 serviceWorkerRegistration.register();
